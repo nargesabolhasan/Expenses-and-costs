78546e7adeb1108fce7040dfeeb7e0656940afbc
Narges.Abolhasan <narges.abolhasan@gmail.com>
2025-06-29 21:35:37 +0330

    add ThemeProvider to app.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-// src/App.js
-import React, { useContext } from "react";
+import React, {useContext} from "react";
 import "./input.css";
-import { ThemeContext, ThemeProvider } from "./context/theme";
+import {ThemeContext, ThemeProvider} from "./context/theme";
 import ThemeToggle from "./components/todo/toggle-theme";
-import TodoList from "./components/todo";
+import AmountManager from "./page/amountManager";
+import {MoneyProvider} from "./context/money";
 
 const App = () => {
-  const { darkStyles } = useContext(ThemeContext);
+    const {darkStyles} = useContext(ThemeContext);
 
-  return (
-    <div
-      className={`min-h-screen flex flex-col items-center justify-center ${darkStyles}`}
+    return (<div
+        className={`min-h-screen flex flex-col items-center gap-2 ${darkStyles}`}
     >
-      <ThemeToggle />
-      <TodoList />
-    </div>
-  );
+        <ThemeToggle/>
+        <MoneyProvider>
+            <AmountManager/>
+        </MoneyProvider>
+    </div>);
 };
 
-const Root = () => (
-  <ThemeProvider>
-    <App />
-  </ThemeProvider>
-);
+const Root = () => (<ThemeProvider>
+    <App/>
+</ThemeProvider>);
 
 export default Root;
